a6ba44417d730ea43916cc65c61999eba3f77042
edm1 <emountjoy@gmail.com>
2018-09-12 17:12:24 +0100

    rerun ld with correct 1000G

--- master.bsub.sh
+++ master.bsub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #BSUB -J ot_ld
-#BSUB -q normal
+#BSUB -q long
 #BSUB -n 16
 #BSUB -R "select[mem>32000] rusage[mem=32000] span[hosts=1]" -M32000
 #BSUB -o output.%J
@@ -12,14 +12,14 @@ set -euo pipefail
 
 # Set args
 cores=16
-# version_date=`date +%y%m%d`
-version_date=180906 # WARNING, this line needs removing!!!!!
+version_date=`date +%y%m%d`
+# version_date=180906 # WARNING, this line needs removing!!!!!
 
 # Load environment
 source activate v2d_data
 
 # Run pipelines
-# snakemake -s 1_make_tables.Snakefile --config version=$version_date --cores 1 --rerun-incomplete
+snakemake -s 1_make_tables.Snakefile --config version=$version_date --cores 1 --rerun-incomplete
 # snakemake -s 2_calculate_LD_table.Snakefile --unlock --config version=$version_date
 snakemake -s 2_calculate_LD_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
 snakemake -s 3_make_overlap_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
