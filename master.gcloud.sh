cd362e1e7d2d36c779f51921ab159a154bb0bdee
edm1 <emountjoy@gmail.com>
2018-09-13 13:15:32 +0100

    full run

--- master.gcloud.sh
+++ master.gcloud.sh
@@ -12,8 +12,8 @@ source activate v2d_data
 # Run pipelines
 snakemake -s 1_make_tables.Snakefile --config version=$version_date --cores 1 --rerun-incomplete
 # snakemake -s 2_calculate_LD_table.Snakefile --unlock --config version=$version_date
-# snakemake -s 2_calculate_LD_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
-# snakemake -s 3_make_overlap_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
+snakemake -s 2_calculate_LD_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
+snakemake -s 3_make_overlap_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
 
 # Copy output to gcs
 gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M rsync -r -x ".*DS_Store$" output gs://genetics-portal-staging/v2d
