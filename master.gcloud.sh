971f09ed5fadec1a8825a0fd8bbbffcd85a38690
edm1 <emountjoy@gmail.com>
2019-03-15 15:11:00 +0000

    scale up

--- master.gcloud.sh
+++ master.gcloud.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -euo pipefail
+# set -euo pipefail
 
 # Set args
 cores=64
@@ -10,9 +10,9 @@ version_date=`date +%y%m%d`
 source activate v2d_data
 
 # Run pipelines
-snakemake -s 1_make_tables.Snakefile --config version=$version_date --cores 1 --rerun-incomplete
+# snakemake -s 1_make_tables.Snakefile --config version=$version_date --cores 1 --rerun-incomplete
 snakemake -s 2_calculate_LD_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
-snakemake -s 3_make_overlap_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
+# snakemake -s 3_make_overlap_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
 
 # Shutdown instance
 gcloud compute instances stop "em-ld" --zone="europe-west1-d"
