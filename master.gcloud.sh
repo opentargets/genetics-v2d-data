0b619f938b5f347c549faf42726848b3f7f71b29
edm1 <emountjoy@gmail.com>
2019-05-21 14:14:35 +0100

    updated master

--- master.gcloud.sh
+++ master.gcloud.sh
@@ -7,14 +7,14 @@ cores=32
 version_date=`date +%y%m%d`
 
 # Load environment
-source activate v2d_data
+conda activate v2d_data
 
 # Run pipelines
 # snakemake -s 1_make_tables.Snakefile --config version=$version_date --cores 1 --rerun-incomplete
 snakemake -s 2_calculate_LD_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
-# snakemake -s 3_make_overlap_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
+snakemake -s 3_make_overlap_table.Snakefile --config version=$version_date --cores $cores --rerun-incomplete
 
 # Shutdown instance
-gcloud compute instances stop "em-ld" --zone="europe-west1-d"
+gcloud compute instances stop "em-v2d" --zone="europe-west1-d"
 
 echo COMPLETE
